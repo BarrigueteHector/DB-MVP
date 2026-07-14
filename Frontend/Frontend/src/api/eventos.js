2805f5d09e0cbfcd2150d9f851fc85b38b99d054
Hector B <hectorrodjls@gmail.com>
2026-07-14 13:20:43 -0600

    v2.5.0 correcciones para la validación de los códigos qr

--- Frontend/Frontend/src/api/eventos.js
+++ Frontend/Frontend/src/api/eventos.js
@@ -6,6 +6,6 @@ const buyTicketRequest = (id) => axios.post(`/comprar_boleto/${id}`);
 const myTicketsRequest = () => axios.get(`/mis_boletos`);
 const createCheckoutSession = (tipo_boleto_id) => axios.post('/create-checkout-session', {tipo_boleto_id});
 const confirmPurchaseRequest = (id) => axios.get(`/confirmar_compra/${id}`);
-const validateTicketRequest = (contenido_qr) => axios.get(`/validar_boleto/${contenido_qr}`);
+const validateTicketRequest = (contenido_qr) => axios.get(`/validar_boleto/${contenido_qr}`, { headers: {'Cache-Control': 'no-cache'}, });
 
 export { listEventsRequest, infoEventRequest, buyTicketRequest, myTicketsRequest, createCheckoutSession, confirmPurchaseRequest, validateTicketRequest }
